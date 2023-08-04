027714e7f160eeb2762caa2019ddc3785090dd66
Jack Nolan <jnolan@recurforever.com>
2023-08-04 14:03:23 -0400

    More work on stats

--- src/components/CombatStats/CombatStatsBurningSection.vue
+++ src/components/CombatStats/CombatStatsBurningSection.vue
@@ -1,3 +1,32 @@
-<template>Burning</template>
+<template>
+  <BaseButton @click="applyBurning" icon="local_fire_department" class="wide"
+    >Apply {{ entityStore.entityAttributes.burning?.val }} Burning
+    Damage</BaseButton
+  >
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useEntityStore } from "@/stores/entity";
+import { adjustAttrsAPI } from "@/utils/attributeUtils";
+import BaseButton from "../Base/BaseButton.vue";
+
+const entityStore = useEntityStore();
+
+const applyBurning = () => {
+  if (!entityStore.entity) {
+    return;
+  }
+  const burning = entityStore.entityAttributes.burning?.val;
+  if (burning && burning > 0) {
+    adjustAttrsAPI(
+      entityStore.entity,
+      entityStore.entityAttributes,
+      {
+        hp: -burning,
+        burning: -Math.min(burning, 3),
+      },
+      `Took ${burning} burning damage`
+    );
+  }
+};
+</script>
